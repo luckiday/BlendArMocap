3b1744a4cd8e66ffc4c929561686d48073ed7855
Yunqi Guo <guoyunqi@gmail.com>
2022-04-12 15:26:03 -0700

    Test the file loader

--- src/cgt_utils/stream.py
+++ src/cgt_utils/stream.py
@@ -55,9 +55,64 @@ class Webcam:
         cv2.destroyAllWindows()
 
 
+class VideoLoader:
+    def __init__(self,
+                 file_directory: str,
+                 title: str = "Stream Detection",
+                 width: int = 640,
+                 height: int = 480):
+        # improved backend for windows
+        
+        self.capture = cv2.VideoCapture(file_directory, cv2.CAP_DSHOW)
+        time.sleep(.25)
+
+        if not self.capture.isOpened():
+            # if backend cannot open capture use random backend
+            self.capture = cv2.VideoCapture(file_directory)
+            time.sleep(.25)
+
+            if not self.capture.isOpened():
+                raise IOError("Cannot open file")
+
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+        self.title = title
+        self.success, self.frame = None, None
+        self.color_spaces = {
+            'rgb': cv2.COLOR_BGR2RGB,
+            'bgr': cv2.COLOR_RGB2BGR
+        }
+
+    def update(self):
+        self.success, self.frame = self.capture.read()
+        # self.frame.flags.writeable = False
+        # self.frame = cv2.flip(frame, 1)
+
+    def set_color_space(self, space):
+        self.frame = cv2.cvtColor(self.frame, self.color_spaces[space])
+
+    def draw(self):
+        cv2.imshow(self.title, self.frame)
+
+    def exit_stream(self):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("ATTEMPT TO EXIT STEAM")
+            return True
+        else:
+            return False
+
+    def __del__(self):
+        print("DEL STREAM")
+        self.capture.release()
+        cv2.destroyAllWindows()
+
+
 def main():
-    stream = Webcam()
-    while stream.capture.isOpened():
+    # stream = Webcam()
+    stream = VideoLoader("test_video.mp4")
+    while stream.success:
         stream.update()
         stream.set_color_space('rgb')
         stream.set_color_space('bgr')
